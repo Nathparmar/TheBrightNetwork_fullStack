0cae0cf5b2c97ee4b231c4518799251a49267fb0
Piraven Nandakumaran <Xothspath@gmail.com>
2023-12-13 14:01:03 +0000

    minor changes

--- the_bright_network_client/src/components/ChatRoomList.js
+++ the_bright_network_client/src/components/ChatRoomList.js
@@ -22,3 +22,8 @@ const ChatRoomList = ({chatRooms, clickChatRoom}) => {
 }
 
 export default ChatRoomList;
+
+
+//         if (!chatRoom.name.includes("Private")){
+    //         <ChatRoom key={chatRoom.id} chatRoomName={chatRoom.name} chatRoomId = {chatRoom.id} clickChatRoom={clickChatRoom}/>
+    //     }
--- the_bright_network_client/src/containers/ChatRoomContainer.js
+++ the_bright_network_client/src/containers/ChatRoomContainer.js
@@ -60,11 +60,10 @@ const ChatRoomContainer = () => {
       
         
     }
-
+    
     const getAllUserChatRooms = async () => {
         const response = await fetch(`http://localhost:8080/chatrooms`);
         const jsonData = await response.json();
-        
         const userChatRooms = jsonData.filter((chatRoom) => {
             const subscriptions = chatRoom.subscriptions || [];
             return subscriptions.some((subscription) => subscription.user.id === clientUser.id);
@@ -129,7 +128,7 @@ const ChatRoomContainer = () => {
             body: JSON.stringify(newChatroom)
         })
         const newChat = await response.json();
-        console.log(newChat.chatroomId);
+        
         setCurrentChatRoom(newChat.chatroomId)
     }
 
@@ -145,7 +144,7 @@ const ChatRoomContainer = () => {
                     chatroomName: `(Private) - ${clientUser.name} and ${messagedUserName} `,
                     userIds: [messagedUserId,clientUser.id]
             })
-            getAllUserChatRooms()
+            //getAllUserChatRooms()
         } else {
             setCurrentChatRoom(privateChats[0].id);
         }
